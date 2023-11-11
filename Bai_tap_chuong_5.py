326cdd9353b084d64692928d81029c3f9f7294bd
Khoa hoc du lieu 17A2 <ktnhung.dhkl17a2hn@sv.uneti.edu.vn>
2023-11-11 23:05:01 +0700

    Bài tập chương 5

---
+++ Bai_tap_chuong_5.py
@@ -0,0 +1,33 @@
+#1.1
+print("**  ** ******    **      **     *******")
+print("**  ** **        **      **     **   **")
+print("****** ******    **      **     **   **")
+print("**  ** **        **      **     **   **")
+print("**  ** ** ****   ******  ******  ******")
+
+#1.2
+x = 10 
+y = 5
+print("x =",x, "y =",y)
+print("Tổng x + y =", x + y )
+print("Hiệu x - y =", x - y)
+print("Tích x*y = ", x*y )
+print("Thương x/y = ", x/y )
+
+
+#1.3 
+so_luong = 5
+don_gia = 25000
+print("Tên hàng: Sữa hộp Vina Milk")
+print("Số lượng:", so_luong)
+print("Đơn giá:", don_gia)
+print("Tiền phải trả:", so_luong * don_gia)
+
+#1.4
+a = float(input("Nhập a ="))
+b = float(input("Nhập b ="))
+c = float(input("Nhập c ="))
+p = (a + b + c)/2
+import math
+print("Diện tích tam giác có số đo", a, b, c, "là :", math.sqrt(p*(p-a)*(p-b)*(p-c)))
+
